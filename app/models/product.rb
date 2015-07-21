dff6d5a3b0ad7c2a85318ec2d958f2ed7a22e8b6
Jonathan Buma <johnny.buma@gmail.com>
2015-07-20 23:25:55 -0700

    Multi-field Search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ActiveRecord::Base
 
   validates :name, :color, :size, :weight, :dimensions, presence: true
-  validates_uniqueness_of :name, :message => "*  [%{value}] has already been recorded, please check database"
+  validates_uniqueness_of :name, :message => "*  [%{value}] has already been recorded, please review database"
 
   def self.search(search)
-    where("Name LIKE ?  OR Dimensions LIKE ?", "%#{search}%", "%#{search}%")
+    where("Id LIKE ? OR Name LIKE ? OR Size LIKE ? OR Color LIKE ? OR Weight LIKE ? OR Dimensions LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
 
   end
 
