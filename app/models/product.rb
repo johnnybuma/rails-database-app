e08dfbb4ff46be4c4861d5213125fe1e1d1dcd7b
Jonathan Buma <johnny.buma@gmail.com>
2015-08-19 18:22:37 +0000

    changed search to order by id instead of created_at desc

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,10 @@ class Product < ActiveRecord::Base
 
   def self.search(search)
     search = search.strip
+    # This syntax is for postgresql for heroku   
     where("Name ILIKE ?  OR Size ILIKE ? OR Color ILIKE ? OR cast(Weight as text) ILIKE ? OR Dimensions ILIKE ? OR Quantity ILIKE ? OR Location ILIKE ? OR Description ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    #This syntax is for sqlite3  
+#    where("Name LIKE ?  OR Size LIKE ? OR Color LIKE ? OR cast(Weight as text) LIKE ? OR Dimensions LIKE ? OR Quantity LIKE ? OR Location LIKE ? OR Description LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
 
   end
 
