6cb4145588a1986af953fb69e3549f714052fec4
johnnybuma <johnny.buma@gmail.com>
2015-10-30 01:25:21 -0700

    changed production domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: 'heroku.com',
+    domain: 'kw-warehouse.herokuapp.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['SENDGRID_USERNAME'],
